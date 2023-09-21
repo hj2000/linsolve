9744e91cd805b664d74cb37db6e17b043e680fbc
hj2000 <2061432792@qq.com>
2023-09-21 13:57:27 +0800

    linsolve 1 cg 2 gmres

---
+++ sparse_solve/include/krylov.h
@@ -0,0 +1,5 @@
+#include <gsl/gsl_spblas.h>
+#include <gsl/gsl_vector.h>
+int cg( gsl_spmatrix* A, gsl_vector* b, gsl_vector* x, double tol );
+int pcg( gsl_spmatrix* A, gsl_vector* b, gsl_vector* x, double tol );
+int gmres( gsl_spmatrix* A, gsl_vector* b, gsl_vector* x, double tol, int maxit );
